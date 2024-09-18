3fde735645de13eab53035ddda44204fa09ff74f
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2024-09-19 01:02:54 +0300

    00

--- app.js
+++ app.js
@@ -1,62 +1,53 @@
 require("dotenv").config();
 
-
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
 const customMorganLogger = require("./lib/morgan");
 const chalk = require("chalk");
-const cors = require('cors');
-var indexRouter = require('./routes/index');
-const { CORS_ENABLED,ALLOWED_DOMAINS } = require("./config/environments");
+const cors = require("cors");
+var indexRouter = require("./routes/index");
+const { CORS_ENABLED, ALLOWED_DOMAINS } = require("./config/environments");
 
 var app = express();
 
-if(CORS_ENABLED){
-  const allowedDomains = ALLOWED_DOMAINS.split(',');
+if (CORS_ENABLED) {
+  const allowedDomains = ALLOWED_DOMAINS.split(",");
 
   const corsOptions = {
     origin: (origin, callback) => {
-    
-     
       if (origin !== undefined || allowedDomains.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
-        console.log("🚀 ~ origin:", origin)
-        callback(new Error('Not allowed by CORS'));
+        console.log("🚀 ~ origin:", origin);
+        callback(new Error("Not allowed by CORS"));
       }
     },
-  };  
+  };
 
   app.use(cors(corsOptions));
-}else{
-
+} else {
   app.use(cors());
 }
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(customMorganLogger);
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api', indexRouter);
-
-
-
+app.use("/api", indexRouter);
 
 // error handler
 app.use(function (err, req, res, next) {
-
   console.log(chalk.red(err));
 
   res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.statusCode).json({code:err.statusCode, error:err});
+  res.status(500).json({ error: err });
 });
 
 module.exports = app;
